d3430fea593a4a840142857fc221d108373cd286
julien ramel <julien.ramel@gmail.com>
2023-08-11 10:13:19 +0200

    buttons

--- web/app/themes/bridge-child/functions.php
+++ web/app/themes/bridge-child/functions.php
@@ -7,3 +7,18 @@ Function qode_child_theme_enqueue_scripts() {
 	wp_enqueue_style( 'childstyle' );
 }
 add_action( 'wp_enqueue_scripts', 'qode_child_theme_enqueue_scripts', 11);
+
+
+function button_shortcode($atts) {
+    $atts = shortcode_atts( array(
+        'target' => '_blank',
+        'hover_type' => 'default',
+        'text' => '',
+        'link' => ''
+    ), $atts );
+
+    $output = '<a href="' . esc_url( $atts['link'] ) . '" target="' . esc_attr( $atts['target'] ) . '" class="mc-submit button-' . esc_attr( $atts['hover_type'] ) . '">' . esc_html( $atts['text'] ) . '</a>';
+
+    return $output;
+}
+add_shortcode( 'button', 'button_shortcode' );
