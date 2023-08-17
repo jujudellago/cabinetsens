40a47302a263d91a3e9c605c49244899d145327b
julien ramel <julien.ramel@gmail.com>
2023-08-17 10:27:41 +0200

    updates

--- web/app/themes/bridge-child/functions.php
+++ web/app/themes/bridge-child/functions.php
@@ -8,17 +8,17 @@ Function qode_child_theme_enqueue_scripts() {
 }
 add_action( 'wp_enqueue_scripts', 'qode_child_theme_enqueue_scripts', 11);
 
-
-function button_shortcode($atts) {
-    $atts = shortcode_atts( array(
-        'target' => '_blank',
-        'hover_type' => 'default',
-        'text' => '',
-        'link' => ''
-    ), $atts );
-
-    $output = '<a href="' . esc_url( $atts['link'] ) . '" target="' . esc_attr( $atts['target'] ) . '" class="mc-submit button-' . esc_attr( $atts['hover_type'] ) . '">' . esc_html( $atts['text'] ) . '</a>';
-
-    return $output;
-}
-add_shortcode( 'button', 'button_shortcode' );
+#
+#function button_shortcode($atts) {
+#    $atts = shortcode_atts( array(
+#        'target' => '_blank',
+#        'hover_type' => 'default',
+#        'text' => '',
+#        'link' => ''
+#    ), $atts );
+#
+#    $output = '<a href="' . esc_url( $atts['link'] ) . '" target="' . esc_attr( $atts['target'] ) . '" class="mc-submit button-' . esc_attr( $atts['hover_type'] ) . '">' . esc_html( $atts['text'] ) . '</a>';
+#
+#    return $output;
+#}
+#add_shortcode( 'button', 'button_shortcode' );
